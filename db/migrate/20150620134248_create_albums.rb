7b0158d7207a88057105166d7b5582a99788c467
Natali <rinykia@gmail.com>
2015-07-15 14:53:15 +0300

    Update

--- db/migrate/20150620134248_create_albums.rb
+++ db/migrate/20150620134248_create_albums.rb
@@ -1,9 +1,9 @@
 class CreateAlbums < ActiveRecord::Migration
   def change
     create_table :albums do |t|
-    	t.string :cover
-    	t.string :title
-    	t.string :artist
+      t.string :cover
+      t.string :title
+      t.string :artist
 
       t.timestamps null: false
     end
--- db/migrate/20150620134342_create_tracks.rb
+++ db/migrate/20150620134342_create_tracks.rb
@@ -1,9 +1,9 @@
 class CreateTracks < ActiveRecord::Migration
   def change
     create_table :tracks do |t|
-    	t.string :name
-    	t.string :minutes
-    	t.references :album
+      t.string :name
+      t.string :minutes
+      t.references :album
 
       t.timestamps null: false
     end
